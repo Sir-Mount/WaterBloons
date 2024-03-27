98ad88ea16df493348ffc48951b1161d2010b2da
Moed Thomas <TR.deMoed@student.han.nl>
2024-03-27 11:43:17 +0100

    Cactussy code works

---
+++ Assets/Scripts/CactusProjectileScript.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusProjectileScript : MonoBehaviour
+{
+    [SerializeField] private float lifeTime;
+    [SerializeField] private float moveSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        moveSpeed = 0.0f;
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //TODO: Do damage
+        }
+    }
+}
---
+++ Assets/Scripts/CactusScript.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CactusScript : MonoBehaviour
+{
+   [SerializeField]
+   private GameObject player;
+
+   [SerializeField] private float shootingMTTH;
+
+   [SerializeField] private GameObject projectilePrefab;
+
+   [SerializeField] private float damageSeconds;
+
+   private enum states
+   {
+      Normal,
+      Damaged,
+      Dead
+   }
+
+   private states currentState = states.Normal;
+
+   private void Start()
+   {
+      StartCoroutine(shoot(0));
+      StartCoroutine(shoot(1));
+   }
+
+   void Update()
+   {
+      if (currentState == states.Dead) return;
+      
+      Vector3 newtarget = player.transform.position;
+      newtarget.y = transform.position.y;
+      transform.LookAt(newtarget);
+   }
+   
+   IEnumerator shoot(int childNumber)
+   {
+      if (currentState == states.Dead) yield break;
+      GameObject newProjectile = Instantiate(projectilePrefab, transform.GetChild(childNumber).transform.position + transform.forward, transform.rotation);
+      
+      Vector3 newtarget = player.transform.position;
+      newProjectile.transform.LookAt(newtarget);
+      
+      yield return new WaitForSeconds(Random.Range(shootingMTTH * 0.8f, shootingMTTH * 1.2f));
+      StartCoroutine(shoot(childNumber));
+   }
+
+   private void OnCollisionEnter(Collision other)
+   {
+      if (other.gameObject.CompareTag("Player"))
+      {
+         if (currentState == states.Normal)
+         {
+            currentState = states.Damaged;
+            StartCoroutine(waitToHeal());
+         }
+         else if (currentState == states.Damaged)
+         {
+            currentState = states.Dead;
+         }
+      }
+   }
+
+   IEnumerator waitToHeal()
+   {
+      yield return new WaitForSeconds(damageSeconds);
+      if (currentState == states.Dead) yield break;
+      
+      currentState = states.Normal;
+   }
+}
